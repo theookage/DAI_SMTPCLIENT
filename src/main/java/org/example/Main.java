30d7ac233b909fa171267dadbbd1546bd081eb26
Théo Coutaudier <theo.coutaudier@heig-vd.ch>
2022-12-01 11:31:51 +0100

    Buffer et socket

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        final int DESTINATION_PORT = 25;
+
+        Socket clientSocket = new Socket("127.0.0.1", DESTINATION_PORT);
+        BufferedReader fromClient = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader fromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
+        BufferedWriter toServer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.UTF_8));
+
+        
+
+        fromServer.close();
+        toServer.close();
+        clientSocket.close();
+
     }
 }
