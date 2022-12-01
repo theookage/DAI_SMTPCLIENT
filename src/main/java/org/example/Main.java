0fc85f48d7f9f6f52845adff52832b755f223b89
Théo Coutaudier <theo.coutaudier@heig-vd.ch>
2022-12-01 13:17:35 +0100

    readfile

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,11 @@ package org.example;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class Main {
+
+
     public static void main(String[] args) throws IOException {
         final int DESTINATION_PORT = 25;
 
@@ -12,12 +15,18 @@ public class Main {
         BufferedReader fromClient = new BufferedReader(new InputStreamReader(System.in));
         BufferedReader fromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
         BufferedWriter toServer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.UTF_8));
+        ReadAllTypeOfFile read = new ReadAllTypeOfFile();
+        List<String> emails = new;
+        emails.add("abc");
+        emails = read.readAndList(emails, "email.txt");
 
-        
+
 
         fromServer.close();
         toServer.close();
         clientSocket.close();
 
     }
+
+
 }
