addc688656d99794545a0d64822945c87ef213c0
Théo Coutaudier <theo.coutaudier@heig-vd.ch>
2022-12-06 20:08:06 +0100

    cool push readfile

--- src/config/ConfigurationManager.java
+++ src/config/ConfigurationManager.java
@@ -9,6 +9,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConfigurationManager {
+
+
+
     private List<String> getLine(File file) throws IOException {
 
         InputStreamReader isr = new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8);
@@ -18,16 +21,36 @@ public class ConfigurationManager {
 
         while(isr.ready()){
 
-            while((c = (char)isr.read()) != '\n')
+            while((c = (char)isr.read()) != '\n' && isr.ready())
                 s.append(c);
             line.add(s.toString());
+            s = new StringBuilder();
         }
 
         return line;
     }
-    public String readConfigAdress(File file) throws IOException {
+    public String getConfigAdress(File file) throws IOException {
 
-        List<String> Line = getLine(file);
+        String[] split;
+        for (String a : getLine(file)){
+            split = a.split("=");
+            if (split[0].equals("ip"))
+                return split[1];
+        }
+        System.out.println("Erreur, pas trouvé d'adresse");
         return "";
     }
+    public String getConfigPort(File file) throws IOException {
+
+        String[] split;
+        for (String a : getLine(file)){
+            split = a.split("=");
+            if (split[0].equals("port"))
+                return split[1];
+        }
+        System.out.println("Erreur, pas trouvé d'adresse");
+        return "";
+    }
+
+
 }
